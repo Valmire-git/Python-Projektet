b794d9ca2f07c073842ab9dd35f7df317f7f656c
Valmire Sejdaj <valmire.sejdaj@gmail.com>
2025-10-14 21:43:09 +0200

    Dua ta shtoj kete ushtrim me Loop Ne Git

---
+++ python lernen/ushtrimeLoop.py
@@ -0,0 +1,21 @@
+for i in range (1,6):
+    print("Numri eshte:",i)
+
+fjalia = "Valmire"
+for shkronja in fjalia:
+    print("shkronja",shkronja)
+
+numri = 1
+while numri <=5:
+    print("Numri eshte:",numri)
+    numri+=1
+
+for i in range (10):
+    if i == 6:
+      continue #kalon vetem numri 6
+print(i) 
+
+for i in range (10):
+    if i ==6:
+      break #Ndal krejt loop-in
+    print(i)
